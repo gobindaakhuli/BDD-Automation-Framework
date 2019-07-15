a07d462e2358076241b40f1eb7cf1fb4b34c2113
Gobinda Akhuli <gobindaakhuli@gmail.com>
2019-07-15 20:37:17 +0530

    Implement log4j for getting customize log file

--- AmazonTest/src/test/java/com/amazon/test/steps/AcceptanceTestSuite.java
+++ AmazonTest/src/test/java/com/amazon/test/steps/AcceptanceTestSuite.java
@@ -1,16 +1,24 @@
 package com.amazon.test.steps;
 
 import java.io.IOException;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.amazon.test.reusable.Reusable;
-
 import net.serenitybdd.jbehave.SerenityStories;
 
 public class AcceptanceTestSuite extends SerenityStories {
+	public static Logger APP_LOGS = LogManager.getLogger(AcceptanceTestSuite.class);
 	public AcceptanceTestSuite() throws IOException {
+		try {
+			APP_LOGS.info("start to fetch properties file");
+			Reusable.initProperties();
+			APP_LOGS.info("Property files loads successfully");
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 		//runSerenity().inASingleSession();
 		//findStoriesCalled() to specify story
-		Reusable.initProperties();
+		
 	}
 
 }
